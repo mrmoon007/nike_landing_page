5314134c97f29625761b0d7e17d2e50455e1d925
Mostafijur <mrmoon025@gmail.com>
2024-01-14 21:55:39 +0600

    [feat] Added assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <main className="relative">
+      <section>
+        hero section
+      </section>
+      <section>
+
+      </section>
+    </main>
   )
 }

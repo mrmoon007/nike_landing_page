2279ce3bb414608cf15000cb287429f669a13019
Mostafijur <mrmoon025@gmail.com>
2024-01-14 22:06:29 +0600

    [feat] Added nav component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import Nav from "./components/Nav";
+
 export default function App() {
   return (
     <main className="relative">
+      <Nav />
       <section>
         hero section
       </section>

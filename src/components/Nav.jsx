d7f774eb49e152bf3271d0dad59770aed3ea5ae2
Mostafijur <mrmoon025@gmail.com>
2024-01-15 21:46:16 +0600

    [feat] Added nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,37 @@
+import { navLinks } from "../constants";
+import { headerLogo } from "../assets/images/";
+import { hamburger } from "../assets/icons";
 
 const Nav = () => {
   return (
-    <div>
-      Nav
-    </div>
-  )
-}
+    <header className="padding-x py-8 absolute z-10 w-full">
+      <nav className="flex justify-between items-center max-container">
+        <a href="/">
+          <img src="{ headerLogo }" alt="logo" width={130} height={29} />
+        </a>
+        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+          {navLinks.map((link) => (
+            <li key={link.label}>
+              <a
+                href="{link.href}"
+                className=" font-montserrat leading-normal text-lg text-slate-gray"
+              >
+                {link.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <div className=" hidden max-lg:block">
+          <img 
+          src="{hamburger}" 
+          alt="Humburgur"
+          height={25}
+          width={25} 
+          />
+        </div>
+      </nav>
+    </header>
+  );
+};
 
-export default Nav
+export default Nav;
